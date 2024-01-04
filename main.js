8ccdf2fba27f1964fc69d6f53b698fa8a886fcc3
Potter <75838259@qq.com>
2024-01-04 14:29:40 +0800

    chore: update version

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // This is free and unencumbered software released into the public domain.
 // See LICENSE for details
 
-const {app, BrowserWindow, Menu} = require('electron');
-const log = require('electron-log');
-const {autoUpdater} = require("electron-updater");
+const { app, BrowserWindow, Menu } = require("electron");
+const log = require("electron-log");
+const { autoUpdater } = require("electron-updater");
 
 //-------------------------------------------------------------------
 // Logging
@@ -14,34 +14,35 @@ const {autoUpdater} = require("electron-updater");
 // but it sure makes debugging easier :)
 //-------------------------------------------------------------------
 autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
+autoUpdater.logger.transports.file.level = "info";
+log.info("App starting...");
 
 //-------------------------------------------------------------------
 // Define the menu
 //
 // THIS SECTION IS NOT REQUIRED
 //-------------------------------------------------------------------
-let template = []
-if (process.platform === 'darwin') {
-  // OS X
-  const name = app.getName();
-  template.unshift({
-    label: name,
-    submenu: [
-      {
-        label: 'About ' + name,
-        role: 'about'
-      },
-      {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click() { app.quit(); }
-      },
-    ]
-  })
+let template = [];
+if (process.platform === "darwin") {
+	// OS X
+	const name = app.getName();
+	template.unshift({
+		label: name,
+		submenu: [
+			{
+				label: "About " + name,
+				role: "about",
+			},
+			{
+				label: "Quit",
+				accelerator: "Command+Q",
+				click() {
+					app.quit();
+				},
+			},
+		],
+	});
 }
-
 
 //-------------------------------------------------------------------
 // Open a window that displays the version
@@ -55,53 +56,59 @@ if (process.platform === 'darwin') {
 let win;
 
 function sendStatusToWindow(text) {
-  log.info(text);
-  win.webContents.send('message', text);
+	log.info(text);
+	win.webContents.send("message", text);
 }
 function createDefaultWindow() {
-  win = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
-  win.webContents.openDevTools();
-  win.on('closed', () => {
-    win = null;
-  });
-  win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
-  return win;
+	win = new BrowserWindow({
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+		},
+	});
+	win.webContents.openDevTools();
+	win.on("closed", () => {
+		win = null;
+	});
+	win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+	return win;
 }
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
-})
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
+autoUpdater.on("checking-for-update", () => {
+	sendStatusToWindow("Checking for update...");
 });
-app.on('ready', function() {
-  // Create the Menu
-  const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
+autoUpdater.on("update-available", (info) => {
+	sendStatusToWindow("Update available.");
+});
+autoUpdater.on("update-not-available", (info) => {
+	sendStatusToWindow("Update not available.");
+});
+autoUpdater.on("error", (err) => {
+	sendStatusToWindow("Error in auto-updater. " + err);
+});
+autoUpdater.on("download-progress", (progressObj) => {
+	let log_message = "Download speed: " + progressObj.bytesPerSecond;
+	log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+	log_message =
+		log_message +
+		" (" +
+		progressObj.transferred +
+		"/" +
+		progressObj.total +
+		")";
+	sendStatusToWindow(log_message);
+});
+autoUpdater.on("update-downloaded", (info) => {
+	sendStatusToWindow("Update downloaded");
+});
+app.on("ready", function () {
+	// Create the Menu
+	const menu = Menu.buildFromTemplate(template);
+	Menu.setApplicationMenu(menu);
 
-  createDefaultWindow();
+	createDefaultWindow();
 });
-app.on('window-all-closed', () => {
-  app.quit();
+app.on("window-all-closed", () => {
+	app.quit();
 });
 
 //
@@ -114,8 +121,8 @@ app.on('window-all-closed', () => {
 // This will immediately download an update, then install when the
 // app quits.
 //-------------------------------------------------------------------
-app.on('ready', function()  {
-  autoUpdater.checkForUpdatesAndNotify();
+app.on("ready", function () {
+	autoUpdater.checkForUpdatesAndNotify();
 });
 
 //-------------------------------------------------------------------
@@ -143,5 +150,5 @@ app.on('ready', function()  {
 // autoUpdater.on('download-progress', (progressObj) => {
 // })
 // autoUpdater.on('update-downloaded', (info) => {
-//   autoUpdater.quitAndInstall();  
+//   autoUpdater.quitAndInstall();
 // })
